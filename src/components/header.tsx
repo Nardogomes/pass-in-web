07303e5bd837942aa6e11a1bf0954c7ffa5da61a
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2024-04-03 21:27:06 -0300

    feat: add component nav-link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import nlwUniteIcon from '../assets/nlw-unite-icon.svg'
+import { Navlink } from './nav-link'
 
 export function Header() {
   return (
@@ -6,8 +7,8 @@ export function Header() {
       <img src={nlwUniteIcon} alt="" />
 
       <nav className="flex gap-5">
-        <a href="#" className="font-medium text-sm text-zinc-300">Eventos</a>
-        <a href="#" className="font-medium text-sm ">Participantes</a>
+        <Navlink href='/eventos' >Eventos</Navlink>
+        <Navlink href='/participantes' >Participantes</Navlink>
       </nav>
     </div>
   )
