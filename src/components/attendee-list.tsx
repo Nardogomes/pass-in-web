6d1df6dddbd8e8130c81b06479b5b5cbf28ac09f
Ednardo Gomes <ednardo@email.com>
2024-04-08 20:58:13 -0300

    feat: saving URL state

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -21,41 +21,91 @@ interface Attendee {
 }
 
 export function AttendeeList() {
-  const [searchInput, setSearchInput] = useState('')
-  const [page, setPage] = useState(1)
+  const [searchInput, setSearchInput] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  })
+
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
+
   const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<Attendee[]>([])
 
   const totalPages = Math.ceil(total / 10)
 
   useEffect(() => {
-    fetch(`http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees?pageIndex=${page - 1}`)
+    const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+
+    url.searchParams.set('pageIndex', String(page - 1))
+
+    if (searchInput.length > 2) {
+      url.searchParams.set('query', searchInput)
+    }
+
+    fetch(url)
       .then(response => response.json())
       .then(data => {
         setAttendees(data.attendees)
         setTotal(data.total)
       })
-  }, [page])
+  }, [page, searchInput])
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('search', search)
+
+    window.history.pushState({}, "", url)
+
+    setSearchInput(search)
+  }
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('page', String(page))
+
+    window.history.pushState({}, "", url)
+
+    setPage(page)
+  }
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearchInput(event.target.value)
-  }
-
-  function goToNextPage() {
-    setPage(page + 1)
-  }
-
-  function goToPreviousPage() {
-    setPage(page - 1)
-  }
-
-  function goToLastPage() {
-    setPage(totalPages)
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
+
+  function goToLastPage() {
+    setCurrentPage(totalPages)
+  }
+
+  function goToPreviousPage() {
+    setCurrentPage(page - 1)
+  }
+
+  function goToNextPage() {
+    setCurrentPage(page + 1)
+  }
+
+
+
 
   return (
     <div className="flex flex-col gap-4">
@@ -64,9 +114,14 @@ export function AttendeeList() {
 
         <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
-          <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" type="text" placeholder="Buscar participante..." />
+          <input
+            onChange={onSearchInputChanged}
+            value={searchInput}
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
+            type="text"
+            placeholder="Buscar participante..."
+          />
         </div>
-        {searchInput}
       </div>
 
       <Table>
